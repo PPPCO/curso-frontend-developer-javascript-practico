04e2a62e8a0068d7e353efaad1fc2cd429952628
Jose Francisco <pepepaco982@gmail.com>
2022-07-26 19:10:06 -0500

    actualizacion de la clase 21 de platzi

--- main.js
+++ main.js
@@ -1,8 +1,42 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu =  document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
+menuHamIcon.addEventListener('click',togglemobileMenu);
+menuCarritoIcon.addEventListener('click',toggleCarritoAside);
 
 function toggleDesktopMenu(){
+    const isAsideOpen = aside.classList.contains('inactive');
+    if(!isAsideOpen){
+        aside.classList.toggle('inactive');
+    }
     desktopMenu.classList.toggle('inactive');
+}
+
+function togglemobileMenu(){
+    const isAsideOpen = aside.classList.contains('inactive');
+    if(!isAsideOpen){
+        aside.classList.toggle('inactive');
+    }
+
+    mobileMenu.classList.toggle('inactive');
+}
+
+
+
+
+function toggleCarritoAside(){
+    const ismobileMenuOpen = mobileMenu.classList.contains('inactive');
+    //aside.classList.toggle('inactive');
+
+    if(!ismobileMenuOpen){
+        mobileMenu.classList.toggle('inactive');
+    }
+
+    aside.classList.toggle('inactive');
 }
